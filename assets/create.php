b0663f2e952200ce1562bf09f209ed45041cb8c6
Jake Giltsoff <contact@designbyjake.co.uk>
2013-03-12 12:58:33 +0000

    Update default menu

--- assets/create.php
+++ assets/create.php
@@ -21,7 +21,7 @@ function create_all_the_things() {
 	add_filter( 'get_search_form', 'create_wpsearch' );
 	add_filter('the_content', 'create_filter_ptags_on_images');
 	add_filter('excerpt_more', 'create_excerpt_more');
-} 
+}
 
 
 // Wordpress Cleanup
@@ -50,20 +50,20 @@ function create_remove_wp_ver_css_js( $src ) {
 }
 
 function create_remove_wp_widget_recent_comments_style() {
-   if ( has_filter('wp_head', 'wp_widget_recent_comments_style') ) {
-	  remove_filter('wp_head', 'wp_widget_recent_comments_style' );
-   }
+	if ( has_filter('wp_head', 'wp_widget_recent_comments_style') ) {
+		remove_filter('wp_head', 'wp_widget_recent_comments_style' );
+	}
 }
 
 function create_remove_recent_comments_style() {
-  global $wp_widget_factory;
-  if (isset($wp_widget_factory->widgets['WP_Widget_Recent_Comments'])) {
-	remove_action('wp_head', array($wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style'));
-  }
+	global $wp_widget_factory;
+	if (isset($wp_widget_factory->widgets['WP_Widget_Recent_Comments'])) {
+		remove_action('wp_head', array($wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style'));
+	}
 }
 
 function create_gallery_style($css) {
-  return preg_replace("!<style type='text/css'>(.*?)</style>!s", '', $css);
+	return preg_replace("!<style type='text/css'>(.*?)</style>!s", '', $css);
 }
 
 
@@ -71,10 +71,10 @@ function create_gallery_style($css) {
 // ------------------------------------------------------------
 
 function create_scripts_and_styles() {
-  if (!is_admin()) {
-	wp_register_style( 'create-stylesheet', get_stylesheet_directory_uri() . '/assets/css/screen.css', array(), '', 'all' );
-	wp_enqueue_style( 'create-stylesheet' );
-  }
+	if (!is_admin()) {
+		wp_register_style( 'create-stylesheet', get_stylesheet_directory_uri() . '/assets/css/screen.css', array(), '', 'all' );
+		wp_enqueue_style( 'create-stylesheet' );
+	}
 }
 
 
@@ -88,7 +88,7 @@ function create_theme_support() {
 	add_theme_support( 'menus' );
 	register_nav_menus(
 		array(
-			'main-nav' => __( 'Header Menu', 'createtheme' ) 
+			'main-nav' => __( 'Header Menu', 'createtheme' )
 		)
 	);
 }
@@ -99,30 +99,30 @@ function create_theme_support() {
 
 function create_main_nav() {
 	wp_nav_menu(array(
-		'container' => false,                              // remove nav container
-		'container_class' => 'menu',                       // class of container (should you choose to use it)
-		'menu' => __( 'Header Menu', 'createtheme' ),      // nav name
-		'menu_class' => 'menu nav top-nav',                // adding custom nav class
-		'theme_location' => 'main-nav',                    // where it's located in the theme
-		'before' => '',                                    // before the menu
-		'after' => '',                                     // after the menu
-		'link_before' => '',                               // before each link
-		'link_after' => '',                                // after each link
-		'depth' => 0,                                      // limit the depth of the nav
-		'fallback_cb' => 'create_main_nav_fallback'        // fallback function
+		'container' => false,
+		'container_class' => 'menu',
+		'menu' => __( 'Header Menu', 'createtheme' ),
+		'menu_class' => 'menu nav top-nav',
+		'theme_location' => 'main-nav',
+		'before' => '',
+		'after' => '',
+		'link_before' => '',
+		'link_after' => '',
+		'depth' => 0,
+		'fallback_cb' => 'create_main_nav_fallback'
 	));
-} 
+}
 
 function create_main_nav_fallback() {
-	wp_page_menu( array(
+	wp_page_menu(array(
 		'show_home' => true,
-	'menu_class' => 'nav clearfix', 
-		'include'     => '',
-		'exclude'     => '',
-		'echo'        => true,
-		'link_before' => '',                      
-		'link_after' => ''                             
-	) );
+		'menu_class' => 'menu nav top-nav',
+		'include' => '',
+		'exclude' => '',
+		'echo' => true,
+		'link_before' => '',
+		'link_after' => ''
+	));
 }
 
 
@@ -130,7 +130,7 @@ function create_main_nav_fallback() {
 // ------------------------------------------------------------
 
 function create_filter_ptags_on_images($content){
-   return preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
+	return preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
 }
 
 function create_excerpt_more($more) {
@@ -150,7 +150,7 @@ function create_get_the_author_posts_link() {
 	$link = sprintf(
 		'<a href="%1$s" title="%2$s" rel="author">%3$s</a>',
 		get_author_posts_url( $authordata->ID, $authordata->user_nicename ),
-		esc_attr( sprintf( __( 'Posts by %s' ), get_the_author() ) ), // No further l10n needed, core will take care of this one
+		esc_attr( sprintf( __( 'Posts by %s' ), get_the_author() ) ),
 		get_the_author()
 	);
 	return $link;
